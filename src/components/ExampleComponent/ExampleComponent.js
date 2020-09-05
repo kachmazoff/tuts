9c008bd559b4abe616d3b4172de5d26f69d2a463
kachmazov <kachmazov@elfin.tech>
2020-09-06 02:24:31 +0400

    Init project

---
+++ src/components/ExampleComponent/ExampleComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExampleComponent(props) {
+  return (
+    <div>
+      Hello, {props.name}
+    </div>
+  )
+}
+
+export { ExampleComponent }
---
+++ src/components/ExampleComponent/ExampleComponent.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ExampleComponent } from './ExampleComponent'
+
+export default {
+  title: 'Components/ExampleComponent',
+  component: ExampleComponent,
+  argTypes: {
+    name: { control: 'text' },
+  },
+}
+
+export const exampleComponent = (args) => <ExampleComponent name={args.name || 'Sasha'} />;
+
+const Template = (args) => <ExampleComponent {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  name: 'Sasha',
+};
