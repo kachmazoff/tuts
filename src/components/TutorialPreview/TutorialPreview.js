342b46cb0c4ae6d405375715f6c50c458fcff89c
kachmazov <kachmazov@elfin.tech>
2020-10-21 16:26:18 +0400

    TutorialPreview as Link

--- src/components/TutorialPreview/TutorialPreview.js
+++ src/components/TutorialPreview/TutorialPreview.js
@@ -5,11 +5,11 @@ import styles from "./styles.module.css";
 export const TutorialPreview = ({ tutorial }) => {
   const { title, description } = tutorial;
   return (
-    <div className={styles.wrapper}>
-      <h5 className={styles.title}>
-        <Link to={`/tutorial/${tutorial.id}`}>{title}</Link>
-      </h5>
-      <p className={styles.description}>{description}</p>
-    </div>
+    <Link to={`/tutorial/${tutorial.id}`} className={styles.link}>
+      <div className={styles.wrapper}>
+        <h5 className={styles.title}>{title}</h5>
+        <p className={styles.description}>{description}</p>
+      </div>
+    </Link>
   );
 };
