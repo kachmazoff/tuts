92c473b5c81d28f1c714c62d2acaa4fbdc66f299
kachmazov <alek.kachmazov@yandex.ru>
2020-10-26 23:13:49 +0400

    Display author on TutorialPreview

--- src/components/TutorialPreview/TutorialPreview.js
+++ src/components/TutorialPreview/TutorialPreview.js
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
 export const TutorialPreview = ({ tutorial }) => {
-  const { title, description } = tutorial;
+  const { title, description, author } = tutorial;
   return (
     <Link to={`/tutorial/${tutorial.id}`} className={styles.link}>
       <div className={styles.wrapper}>
         <h5 className={styles.title}>{title}</h5>
         <p className={styles.description}>{description}</p>
+        <span className={styles.author}>
+          @{(author || {}).username || "unknown404"}
+        </span>
       </div>
     </Link>
   );
