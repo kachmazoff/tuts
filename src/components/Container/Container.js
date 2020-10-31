6acd0c74c888a06e033e0299b0ddefd6b04944ca
kachmazov <alek.kachmazov@yandex.ru>
2020-10-31 23:45:13 +0400

    Add prop typing for Container

--- src/components/Container/Container.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./styles.module.css";
-
-export const Container = ({ children, fullWidth, className }) => (
-  <div className={[styles.wrapper, className].join(" ")}>{children}</div>
-);
